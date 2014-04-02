1b2b75111c84e47da73987e62b6046decf2224ad
Tumelo <tumelophatuli@ymail.com>
2014-04-02 08:31:21 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SemesterProject::Application.config.secret_key_base = 'd0c930b3d6914d8561a62c75f8f3507ae4d30698781ebdd58d5e0d16cf03512ca91277a069d1ea67c5e12b5a414163a5c66eb25f8e155327f20da8da36048e9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SemesterProject::Application.config.session_store :cookie_store, key: '_Semester_project_session'
